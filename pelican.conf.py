c1d80dca322fe6d137015e625bcbeb6ed8e95f6e
rsegebre <rsegebre@colgate.edu>
2012-06-11 20:16:21 -0400

    generated pelican content

---
+++ pelican.conf.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = u"Roberto Segebre"
+SITENAME = u"rsegebre"
+SITEURL = 'http://rsegebre.github.com'
+
+TIMEZONE = 'America/New_York'
+
+
+GITHUB_URL = 'http://github.com/rsegebre'
+DEFAULT_LANG='en'
+THEME = 'notmyidea'
+
+# Blogroll
+LINKS =  (
+    ('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
+    ('Python.org', 'http://python.org'),
+    ('Jinja2', 'http://jinja.pocoo.org'),
+    ('You can modify those links in your config file', '#')
+         )
+
+# Social widget
+SOCIAL = (
+          ('You can add links in your config file', '#'),
+         )
+
+DEFAULT_PAGINATION = 7
+
+
+    
